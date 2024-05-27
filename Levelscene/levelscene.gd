f6a06bc7901cbe15158ef3ae442aeb3374852281
dinesh1160 <dinesharunachalam116@mail.com>
2024-05-27 14:21:45 +0530

    added platforms, camera bugs resolved

---
+++ Levelscene/levelscene.gd
@@ -0,0 +1,18 @@
+extends Node2D
+@onready var camera_2d = $Camera2D
+@onready var player = $Player
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func _physics_process(delta):
+	camera_2d.position = player.position
+	
+	
